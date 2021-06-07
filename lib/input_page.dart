98df0b9d780565819768756aeb407ef8135e5bb8
aeladrin <andriy.krupych@gmail.com>
2021-06-07 20:00:42 +0300

    buttons

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,10 +1,16 @@
 import 'package:bmi_calculator/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const buttonHeight = 80.0;
+const iconWidth = 50.0;
+const bigMargin = 25.0;
+const smallMargin = 5.0;
 
 const cardColor = '#111428';
 const selectedCardColor = '#1d1f33';
+const textColor = '#8d8e98';
+const selectedTextColor = '#ffffff';
 const buttonColor = '#eb1555';
 
 class InputPage extends StatefulWidget {
@@ -19,32 +25,88 @@ class _InputPageState extends State<InputPage> {
           title: Text('BMI CALCULATOR'),
         ),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            SizedBox(
+              height: bigMargin,
+            ),
             Expanded(
               child: Row(
                 children: [
-                  ReusableCard(
-                    color: HexColor(selectedCardColor),
+                  SizedBox(
+                    width: bigMargin,
                   ),
-                  ReusableCard(
-                    color: HexColor(selectedCardColor),
+                  GenderButton(
+                    icon: FontAwesomeIcons.mars,
+                    text: 'MALE',
+                    isSelected: true,
+                  ),
+                  SizedBox(
+                    width: smallMargin,
+                  ),
+                  GenderButton(
+                    icon: FontAwesomeIcons.venus,
+                    text: 'FEMALE',
+                    isSelected: false,
+                  ),
+                  SizedBox(
+                    width: bigMargin,
                   ),
                 ],
               ),
             ),
-            ReusableCard(),
+            SizedBox(
+              height: bigMargin,
+            ),
             Expanded(
               child: Row(
                 children: [
+                  SizedBox(
+                    width: bigMargin,
+                  ),
                   ReusableCard(),
-                  ReusableCard(),
+                  SizedBox(
+                    width: bigMargin,
+                  ),
                 ],
               ),
+            ),
+            SizedBox(
+              height: bigMargin,
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: bigMargin,
+                  ),
+                  ReusableCard(),
+                  SizedBox(
+                    width: smallMargin,
+                  ),
+                  ReusableCard(),
+                  SizedBox(
+                    width: bigMargin,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: bigMargin,
             ),
             Container(
               color: HexColor(buttonColor),
               margin: EdgeInsets.only(top: 10),
               height: buttonHeight,
+              child: Center(
+                  child: Text(
+                "CALCULATE YOUR BMI",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 3,
+                ),
+              )),
             )
           ],
         ),
@@ -52,20 +114,55 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
+  final Widget child;
   final Color color;
 
-  ReusableCard({this.color});
+  ReusableCard({this.child, this.color});
 
   @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        margin: EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: color ?? HexColor(cardColor),
+  Widget build(BuildContext context) => Expanded(
+        child: Container(
+          child: child,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: color ?? HexColor(cardColor),
+          ),
         ),
-      ),
-    );
-  }
+      );
+}
+
+class GenderButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final bool isSelected;
+
+  GenderButton({this.icon, this.text, this.isSelected});
+
+  Color getForegroundColor() =>
+      HexColor(isSelected ? selectedTextColor : textColor);
+
+  @override
+  Widget build(BuildContext context) => ReusableCard(
+        color: HexColor(selectedCardColor),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              color: getForegroundColor(),
+              size: iconWidth,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: Text(
+                text,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: getForegroundColor(),
+                ),
+              ),
+            )
+          ],
+        ),
+      );
 }
